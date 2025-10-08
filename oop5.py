eb4c3daa45daa95c5a18199d25a414644d440776
Corazon0D <korazon@gmail.com>
2025-10-08 13:13:24 +0300

    OOP Специальные методы

---
+++ oop5.py
@@ -0,0 +1,54 @@
+# Основные понятия в ООП
+# Специальные методы
+# __init__ - создание объекта
+# __name__ - возвращает имя объекта (экземпляра) или модуля
+# __sizeof__() - размер объекта в байтах
+# __class__ - имя класса объекта
+
+# Всё есть объект - манипуляции с объектом (примеры)
+# a, b = 3, 4
+#
+# c = a + b # "под капотом" -> a.__add__(b)
+# c = a / b  # "под капотом" -> a.__div__(b)
+# c = int(a) # "под капотом" -> a.__str__()
+
+class A:
+    def __init__(self, a=0):
+        self.a = a
+
+    # Переопределяем (override) преобразование в строку
+    def __str__(self):
+        return f'<Объект класса А со значением а = {self.a}>'
+
+    # Строковое представление (representation) сложного объекта
+    def __repr__(self):
+        return f'<Объект класса А со значением а = {self.a}>'
+
+    def __add__(self, other):
+        return A(self.a + other.a)
+
+
+class B:
+    def __init__(self, b=0):
+        self.b = b
+
+    # Переопределяем (override) преобразование в строку
+    def __str__(self):
+        return f'<Объект класса B со значением b = {self.b}>'
+
+    # Строковое представление (representation) сложного объекта
+    def __repr__(self):
+        return f'<Объект класса B со значением b = {self.b}>'
+
+    def __sub__(self, other):
+        return B(self.b - other.b)
+
+
+a1 = A(10)
+a2 = A(15)
+c = a1 + a2
+print(c)
+b1 = B(15)
+b2 = B(10)
+d = b1 - b2
+print(d)
